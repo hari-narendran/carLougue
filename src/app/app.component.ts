32f1a3d722ac91b6f982dd7bbd187f60a022a417
hari-narendran <68090130+hari-narendran@users.noreply.github.com>
2024-02-18 18:29:35 -0500

    removing unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodosComponent } from './todos/todos.component';
 import { CommonModule } from '@angular/common';
 import {  } from '@angular/platform-browser';
 import { CarsSearchComponent } from './cars-search/cars-search.component';
@@ -8,7 +7,7 @@ import { CarsSearchComponent } from './cars-search/cars-search.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TodosComponent, CarsSearchComponent],
+  imports: [CommonModule, RouterOutlet, CarsSearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
