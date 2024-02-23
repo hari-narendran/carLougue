4e0b92f4eff96aed163b45d4c99574d210c35fd8
hari-narendran <68090130+hari-narendran@users.noreply.github.com>
2024-02-22 19:40:59 -0500

    added Angular material. added tabs to switch between searching and viewing trips

--- src/app/cars-search/cars-search.component.ts
+++ src/app/cars-search/cars-search.component.ts
@@ -1,20 +1,24 @@
 import { Component, inject } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { CarService } from './services/car.service';
 import { CarmakesService } from './services/carmakes.service';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TripDetailsComponent } from './components/trip-details/trip-details.component';
 
 @Component({
   selector: 'app-cars-search',
   standalone: true,
-  imports: [CommonModule,HeaderComponent, SearchResultsComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    SearchResultsComponent,
+    TripDetailsComponent,
+    MatTabsModule,
+  ],
   providers: [CarService, CarmakesService],
   templateUrl: './cars-search.component.html',
-  styleUrl: './cars-search.component.css'
+  styleUrl: './cars-search.component.css',
 })
-export class CarsSearchComponent {
-
-  
-
-}
+export class CarsSearchComponent {}
